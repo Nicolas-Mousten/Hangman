396d55c2eb54795ae31481cc9f154fbab42d9be6
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-10-29 13:00:28 +0200

    The game can save now, still ned some finetuning

--- Java Code/src/Hangman.java
+++ Java Code/src/Hangman.java
@@ -1,7 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
